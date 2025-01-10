7d076575b437a6afaf96c1a49d8084e4386d43ff
KimMinWoo <kimminou0306@naver.com>
2025-01-10 15:52:18 +0900

    merge

--- src/main/webapp/resources/static/js/slide_splide.js
+++ src/main/webapp/resources/static/js/slide_splide.js
@@ -10,23 +10,23 @@ document.addEventListener('DOMContentLoaded', async function () {
 			let add_html = '';
 			response.forEach(function(img_url){
 				add_html +=  "<li class='splide__slide'><img src='"+ img_url + "' alt='슬라이드 1'></li>"
-			})
+			});
 			console.log(add_html);
 			$('.splide__list').append(add_html);
 
 		},
 		error: function(error) {
 		}
-	});
+	})
 	
 	
 	new Splide('#image-slider', {
 	    type       : 'loop',
 	    perPage    : 2,
-	    autoplay  : true,
-		width : '400px',
-		height   : '300px',
-		pagination : true,
-		cover : true,
-	  }).mount();
+	    autoplay   : true,
+	    width      : '400px',
+	    height     : '300px',
+	    pagination : true,
+	    cover      : true
+	}).mount();
 });
